c1766389b677e6d320d4e985e9e1b6931274b594
mundus <mundus@pc6.home>
2024-01-10 13:59:03 +0100

    Statically added sample products in migrate.js, can add products, can list products

--- backend/src/bin/migrate.js
+++ backend/src/bin/migrate.js
@@ -1,2 +1,16 @@
 var db = require('../modules/access_api/database.js');
-db.sequelize.sync();
+
+db.sequelize.sync()
+    //.then(db.sequelize.drop)
+
+// enforce static category list in the categories table in the database
+    .then(() => db.Category.findOrCreate({where: {name: "Electronics"}}))
+    .then(() => db.Category.findOrCreate({where: {name: "Food"}}))
+    .then(() => db.Category.findOrCreate({where: {name: "Drink"}}))
+    .then(() => db.Category.findOrCreate({where: {name: "Clothes"}}))
+    .then(() => db.Category.findOrCreate({where: {name: "Furniture"}}))
+    .then(() => db.Category.findOrCreate({where: {name: "Other"}})) 
+// enforce static admin user in the database
+    //.then(() => { db.Person.findOrCreate({where: {username: "admin", password: "admin", email: "", phone_number: ""}}); })
+    .then(() => console.log("Database setup successfull"))
+    .catch((err) => console.log("!! migrate.js error: " + err.message));
